3066da5342f979ffd6f1592baf49a914d2a9e102
dada0613 <117885969+dada0613@users.noreply.github.com>
2025-04-20 03:19:23 -0400

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-  message: { type: String, required: true },
+  title: { type: String, required: true },
+  openedContent: { type: String, required: true },
   openDate: { type: Date, required: true },
   createdAt: { type: Date, default: Date.now },
+  mood: { type: String, default: "happy" },
+  opened: { type: Boolean, default: false },
+  wasOpened: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model("Message", messageSchema);
